f68f72643f8fca21c7b9f18a69842da483941f09
Jeremy Fields <jeremy.fields@viget.com>
2015-03-23 09:13:51 -0600

    Initial commit

---
+++ lte-IE8.js
@@ -0,0 +1,94 @@
+//addEventListener polyfill 1.0 / Eirik Backer / MIT Licence
+(function(win, doc){
+	if(win.addEventListener)return;		//No need to polyfill
+
+	function docHijack(p){var old = doc[p];doc[p] = function(v){return addListen(old(v))}}
+	function addEvent(on, fn, self){
+		return (self = this).attachEvent('on' + on, function(e){
+			var e = e || win.event;
+			e.preventDefault  = e.preventDefault  || function(){e.returnValue = false}
+			e.stopPropagation = e.stopPropagation || function(){e.cancelBubble = true}
+			fn.call(self, e);
+		});
+	}
+	function addListen(obj, i){
+		if(i = obj.length)while(i--)obj[i].addEventListener = addEvent;
+		else obj.addEventListener = addEvent;
+		return obj;
+	}
+
+	addListen([doc, win]);
+	if('Element' in win)win.Element.prototype.addEventListener = addEvent;			//IE8
+	else{																			//IE < 8
+		doc.attachEvent('onreadystatechange', function(){addListen(doc.all)});		//Make sure we also init at domReady
+		docHijack('getElementsByTagName');
+		docHijack('getElementById');
+		docHijack('createElement');
+		addListen(doc.all);
+	}
+})(window, document);
+;// Production steps of ECMA-262, Edition 5, 15.4.4.14
+// Reference: http://es5.github.io/#x15.4.4.14
+if (!Array.prototype.indexOf) {
+  Array.prototype.indexOf = function(searchElement, fromIndex) {
+
+    var k;
+
+    // 1. Let O be the result of calling ToObject passing
+    //    the this value as the argument.
+    if (this == null) {
+      throw new TypeError('"this" is null or not defined');
+    }
+
+    var O = Object(this);
+
+    // 2. Let lenValue be the result of calling the Get
+    //    internal method of O with the argument "length".
+    // 3. Let len be ToUint32(lenValue).
+    var len = O.length >>> 0;
+
+    // 4. If len is 0, return -1.
+    if (len === 0) {
+      return -1;
+    }
+
+    // 5. If argument fromIndex was passed let n be
+    //    ToInteger(fromIndex); else let n be 0.
+    var n = +fromIndex || 0;
+
+    if (Math.abs(n) === Infinity) {
+      n = 0;
+    }
+
+    // 6. If n >= len, return -1.
+    if (n >= len) {
+      return -1;
+    }
+
+    // 7. If n >= 0, then Let k be n.
+    // 8. Else, n<0, Let k be len - abs(n).
+    //    If k is less than 0, then let k be 0.
+    k = Math.max(n >= 0 ? n : len - Math.abs(n), 0);
+
+    // 9. Repeat, while k < len
+    while (k < len) {
+      // a. Let Pk be ToString(k).
+      //   This is implicit for LHS operands of the in operator
+      // b. Let kPresent be the result of calling the
+      //    HasProperty internal method of O with argument Pk.
+      //   This step can be combined with c
+      // c. If kPresent is true, then
+      //    i.  Let elementK be the result of calling the Get
+      //        internal method of O with the argument ToString(k).
+      //   ii.  Let same be the result of applying the
+      //        Strict Equality Comparison Algorithm to
+      //        searchElement and elementK.
+      //  iii.  If same is true, return k.
+      if (k in O && O[k] === searchElement) {
+        return k;
+      }
+      k++;
+    }
+    return -1;
+  };
+}
--- what-input.js
+++ what-input.js
@@ -10,20 +10,97 @@
     // like Node.
     module.exports = factory();
   } else {
-    root['WhatInput'] = factory();
+    root['whatInput'] = factory();
   }
 }(this, function () {
 
-/*! what-input.js v 0.0.1 | Author: Jeremy Fields [jeremy.fields@viget.com], 2015 | License: MIT */
+/*
+ * variables
+ */
 
+// cache document.body
+var body = document.body;
+
+//
+var buffer = false;
 var currentInput = null;
+var inputMap = {
+  'keydown': 'keyboard',
+  'mousedown': 'mouse',
+  'touchstart': 'touch',
+  'pointerdown': 'pointer',
+  'MSPointerDown': 'pointer'
+};
+var inputTypes = [];
+var timer;
 
-function setInput(value) {
-  currentInput = value;
+
+/*
+ * functions
+ */
+
+function bufferInput(event) {
+  clearTimeout(timer);
+
+  setInput(inputMap[event.type]);
+
+  buffer = true;
+  timer = setTimeout(function() {
+    buffer = false;
+  }, 1000);
 }
 
+function regularInput(event) {
+  if (!buffer) setInput( inputMap[event.type] );
+}
+
+function setInput(value) {
+  if (currentInput !== value) {
+    currentInput = value;
+    body.setAttribute('data-whatinput', currentInput);
+
+    if (inputTypes.indexOf(currentInput) === -1) inputTypes.push(currentInput);
+  }
+}
+
+
+/*
+ * init
+ */
+
+(function() {
+
+  // touch
+  if ('ontouchstart' in document.documentElement) body.addEventListener('touchstart', bufferInput);
+
+  // pointer/mouse
+  if (window.PointerEvent) {
+    body.addEventListener('pointerdown', regularInput);
+  } else if (window.MSPointerEvent) {
+    body.addEventListener('MSPointerDown', regularInput);
+  } else {
+    body.addEventListener('mousedown', regularInput);
+  }
+
+  // keyboard
+  body.addEventListener('keydown', regularInput);
+
+})();
+
+
+/*
+ * api
+ */
+
 return {
+
+  // returns a string of the current input type
   ask: function() { return currentInput; },
+
+  // returns an array of all the detected input types
+  types: function() { return inputTypes; },
+
+  // manually set the input type
   set: setInput
 };
 
