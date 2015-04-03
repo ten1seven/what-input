ad8b2666bc9303d82fc802008ebcb95af1ac98ba
Jeremy Fields <jeremy.fields@viget.com>
2015-04-03 16:31:49 -0600

    Update watch.js

--- grunt/watch.js
+++ grunt/watch.js
@@ -2,18 +2,12 @@ module.exports = {
   configFiles: {
     files: [
       'Gruntfile.js'
-    ],
-    options: {
-      reload: true
-    }
+    ]
   },
   scripts: {
     files: [
       './src/*.js'
     ],
-    tasks: ['scripts'],
-    options: {
-      spawn: false
-    }
+    tasks: ['scripts']
   }
 };
