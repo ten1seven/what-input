223c0ef8d8340bb2355466d26c1cbf8af5e3545c
Jeremy Fields <jeremy.fields@viget.com>
2015-10-15 14:05:48 -0600

    Manually update AMD wrapper

--- what-input.js
+++ what-input.js
@@ -1,12 +1,18 @@
 ;(function(root, factory) {
   if (typeof define === 'function' && define.amd) {
+
     define([], function() {
       return (factory());
     });
+
   } else if (typeof exports === 'object') {
+
     module.exports = factory();
+
   } else {
+
     root.whatInput = factory();
+    
   }
 } (this, function() {
   'use strict';
