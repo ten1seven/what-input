b7ee7dc7fe8c0eb39edb73a133509685ed082aee
Jeremy Fields <jeremy.fields@viget.com>
2016-02-17 11:37:43 -0700

    Add header comment to minified file

--- gulpfile.js/tasks/scripts.js
+++ gulpfile.js/tasks/scripts.js
@@ -4,12 +4,22 @@ var notify = require('gulp-notify');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
 var concat = require('gulp-concat');
+var header = require('gulp-header');
+var pkg = require('../../package.json');
+var banner = ['/**',
+  ' * <%= pkg.name %> - <%= pkg.description %>',
+  ' * @version v<%= pkg.version %>',
+  ' * @link <%= pkg.homepage %>',
+  ' * @license <%= pkg.license %>',
+  ' */',
+  ''].join('\n');
 
 gulp.task('scripts-uglify', function() {
   return gulp.src(['./what-input.js'])
     .pipe(plumber({errorHandler: notify.onError("Error: <%= error.message %>")}))
     .pipe(uglify())
     .pipe(rename('what-input.min.js'))
+    .pipe(header(banner, { pkg : pkg } ))
     .pipe(gulp.dest('./'))
     .pipe(notify('Scripts uglify task complete'));
 });
