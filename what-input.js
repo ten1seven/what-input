ced3b8c3d28490ac7fda692d90bfaa7d1cee164e
Phil Wolstenholme <phil@wolstenhol.me>
2016-02-29 22:35:11 +0000

    Ignores Apple command keys (and Windows menu keys) because they're used for both mouse and keyboard actions

--- what-input.js
+++ what-input.js
@@ -40,7 +40,9 @@ window.whatInput = (function() {
   var ignoreMap = [
     16, // shift
     17, // control
-    18  // alt
+    18, // alt
+    91, // Windows key / left Apple cmd
+    93  // Windows menu / right Apple cmd
   ];
 
   // mapping of events to input types
