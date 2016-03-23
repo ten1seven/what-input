f6661c4fda529c0be4ec94a312d582db63bad49a
Jeremy Fields <jeremy.fields@viget.com>
2016-03-23 11:46:04 -0600

    Better .DS_Store cleanup

--- gulpfile.js/tasks/clean.js
+++ gulpfile.js/tasks/clean.js
@@ -2,11 +2,5 @@ var gulp = require('gulp');
 var del = require('del');
 
 gulp.task('clean', function () {
-  return del([
-    './.DS_Store',
-    './src/.DS_Store',
-    './src/**/.DS_Store',
-    './gulpfile.js/.DS_Store',
-    './gulpfile.js/**/.DS_Store'
-  ]);
+  return del(['**/.DS_Store']);
 });
