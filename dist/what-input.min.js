2621e00cbbf4c90b8260beee091f7afb63c1bdd9
Jeremy Fields <jeremy.fields@viget.com>
2016-10-30 17:56:17 -0600

    Version bump

--- dist/what-input.min.js
+++ dist/what-input.min.js
@@ -1,6 +1,6 @@
 /**
  * what-input - A global utility for tracking the current input method (mouse, keyboard or touch).
- * @version v4.0.2
+ * @version v4.0.3
  * @link https://github.com/ten1seven/what-input
  * @license MIT
  */
