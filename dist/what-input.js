3f73db9375e3c86f50143c4fbe5f973a1c8f350d
Jeremy Fields <jeremy.fields@viget.com>
2017-01-12 08:38:18 -0700

    Adding check for nodeName, ref #38

--- dist/what-input.js
+++ dist/what-input.js
@@ -192,9 +192,11 @@ return /******/ (function(modules) { // webpackBootstrap
 	        currentIntent !== value
 	      ) {
 
+	        var activeElem = document.activeElement;
 	        var activeInput = (
-	          document.activeElement &&
-	          formInputs.indexOf(document.activeElement.nodeName.toLowerCase()) === -1
+	          activeElem &&
+	          activeElem.nodeName &&
+	          formInputs.indexOf(activeElem.nodeName.toLowerCase()) === -1
 	        ) ? true : false;
 
 	        if (
