56a6a5c8838d31cd608ca644ad55a66157b8b4d0
Jeremy Fields <jeremy.fields@viget.com>
2017-06-13 15:33:27 -0600

    Change that to a minor version bump

--- dist/what-input.min.js
+++ dist/what-input.min.js
@@ -1,6 +1,6 @@
 /**
  * what-input - A global utility for tracking the current input method (mouse, keyboard or touch).
- * @version v4.1.7
+ * @version v4.2.0
  * @link https://github.com/ten1seven/what-input
  * @license MIT
  */
