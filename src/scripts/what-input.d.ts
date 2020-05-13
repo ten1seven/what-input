77edd1a7ce56996a28be3f8e6f1a10c177a29511
Dan Tello <dtello@medallia.com>
2020-05-13 17:08:15 -0400

    Add TypeScript definitions

---
+++ src/scripts/what-input.d.ts
@@ -0,0 +1,14 @@
+declare module "what-input" {
+  export interface WhatInput {
+    ask: (strategy?: "intent") => "pointer" | "keyboard" | "mouse" | "touch";
+    element: () => string | null;
+    ignoreKeys: (keyCodes: number[]) => void;
+    specificKeys: (keyCodes: number[]) => void;
+    registerOnChange: (callback: () => void, strategy?: "intent") => void;
+    unRegisterOnChange: (callback: () => void) => void;
+    clearStorage: () => void;
+  }
+
+  const whatInput: WhatInput;
+  export default whatInput;
+}
