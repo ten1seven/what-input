496b69901bb189849957df5dbbe8e8095409b505
Mihkel Eidast <mihkel@play.ee>
2020-05-31 16:41:04 +0300

    improve types

--- src/scripts/what-input.d.ts
+++ src/scripts/what-input.d.ts
@@ -1,11 +1,15 @@
 declare const whatInput: {
-  ask: (strategy?: "intent") => "pointer" | "keyboard" | "mouse" | "touch";
+  ask: (strategy?: Strategy) => InputMethod;
   element: () => string | null;
   ignoreKeys: (keyCodes: number[]) => void;
   specificKeys: (keyCodes: number[]) => void;
-  registerOnChange: (callback: () => void, strategy?: "intent") => void;
-  unRegisterOnChange: (callback: () => void) => void;
+  registerOnChange: (callback: (type: InputMethod) => void, strategy?: Strategy) => void;
+  unRegisterOnChange: (callback: (type: InputMethod) => void) => void;
   clearStorage: () => void;
 };
+
+export type InputMethod = "initial" | "pointer" | "keyboard" | "mouse" | "touch";
+
+export type Strategy = "input" | "intent";
 
 export default whatInput;
